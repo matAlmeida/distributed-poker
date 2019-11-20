085dd5855e53649e089cf34c176c991c22d084c7
Matheus Almeida <mat.almeida@live.com>
2019-11-20 16:57:13 -0300

    add deck

---
+++ distributed_poker/base/deck.py
@@ -0,0 +1,31 @@
+from distributed_poker.base import Card
+import numpy
+
+class Deck:
+    def __init__(self):
+        self.cards = []
+        self.cardsInGame = []
+        self.Shuffle()
+
+    def Shuffle(self):
+        values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'A', 'J', 'Q', 'K']
+        hearts = [Card('hearts', value) for value in values]
+        spades = [Card('spades', value) for value in values]
+        clubs = [Card('clubs', value) for value in values]
+        dimonds = [Card('dimonds', value) for value in values]
+
+        self.cards = hearts + spades + clubs + dimonds
+        self.cardsInGame = []
+        numpy.random.shuffle(self.cards)
+
+    def draw(self, numberOfDraws=1):
+        draws = []
+        for drawNumber in range(numberOfDraws):
+            drawedCard = self.cards.pop()
+            draws.append(drawedCard)
+            self.cardsInGame.append(drawedCard)
+
+        return draws
+
+    def isEmpty(self):
+        return len(self.cards) == 0
